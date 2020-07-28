00173338ffe21a7b9e93178dc80337c639bd36bd
mohsen afshar <mohsenafshar_ph@yahoo.com>
2020-07-28 16:15:23 +0430

    initial commit

---
+++ Runtime/MyTestScript.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTestScript : MonoBehaviour
+{
+    public static string Name = "Mohsen afshar";
+}
